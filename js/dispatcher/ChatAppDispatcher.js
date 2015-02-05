01676ec249d66058a15b2d56890194f83de9645a
shall <hall.steven.g@gmail.com>
2015-02-05 09:54:05 -0800

    switch subunit dispatcher

--- js/dispatcher/ChatAppDispatcher.js
+++ js/dispatcher/ChatAppDispatcher.js
@@ -14,34 +14,26 @@ var ChatConstants = require('../constants/ChatConstants');
 var Dispatcher = require('flux').Dispatcher;
 var assign = require('object-assign');
 
-var PayloadSources = ChatConstants.PayloadSources;
+var PayloadSources = {view: 'VIEW_ACTION', server: 'SERVER_ACTION'};
+ 
 
-var ChatAppDispatcher = assign(new Dispatcher(), {
+var ChatAppDispatcher = SubUnit.createDispatcher();
 
-  /**
-   * @param {object} action The details of the action, including the action's
-   * type and additional data coming from the server.
-   */
-  handleServerAction: function (action) {
-    var payload = {
-      source: PayloadSources.SERVER_ACTION,
-      action: action
-    };
-    this.dispatch(payload);
-  },
+ChatAppDispatcher.handleServerAction = function (action) {
+  var payload = {
+    source: PayloadSources.server,
+    action: action
+  };
+  this.dispatch(payload);
+};
 
-  /**
-   * @param {object} action The details of the action, including the action's
-   * type and additional data coming from the view.
-   */
-  handleViewAction: function (action) {
-    var payload = {
-      source: PayloadSources.VIEW_ACTION,
-      action: action
-    };
-    this.dispatch(payload);
-  }
+ChatAppDispatcher.handleViewAction = function (action) {
+  var payload = {
+    source: PayloadSources.view,
+    action: action
+  };
+  this.dispatch(payload);
+}
 
-});
 
 module.exports = ChatAppDispatcher;
