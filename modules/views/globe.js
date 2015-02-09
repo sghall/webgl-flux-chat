eb2c55c2beb59dd1ec76cce5cf0d683f4494666c
shall <hall.steven.g@gmail.com>
2015-02-08 16:14:24 -0800

    add two spheres

---
+++ modules/views/globe.js
@@ -0,0 +1,37 @@
+import { scene } from '../scene';
+import { messageStore } from '../stores/message';
+import { threadStore } from '../stores/thread';
+
+function getStateFromStores() {
+  return {
+    messages: messageStore.getAllForCurrentThread(),
+    thread: threadStore.getCurrent()
+  };
+}
+
+export var globeView = SubUnit.createView(scene, {
+  getInitialState: function () {
+    return getStateFromStores();
+  },
+  render: function () {
+    this.root.node().position.set(0, 0, 500)
+
+    this.root.append("mesh")
+      .attr("material", new THREE.MeshPhongMaterial({color: 'blue', shininess: 100}))
+      .attr("geometry", new THREE.SphereGeometry(60, 30, 30))
+      .each(function () {
+        this.position.set(400, 100, 0);
+      })
+
+    this.root.append("mesh")
+      .attr("material", new THREE.MeshPhongMaterial({color: 'red', shininess: 100}))
+      .attr("geometry", new THREE.SphereGeometry(60, 30, 30))
+      .each(function () {
+        this.position.set(-400, 100, 0);
+      })
+  }
+});
+
+function onStoreUpdate() {
+  view.setState(getStateFromStores());
+}
