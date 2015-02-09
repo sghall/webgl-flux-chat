228705f7122070784b96e3fc2bc2cddf3f902fab
shall <hall.steven.g@gmail.com>
2015-02-08 16:13:48 -0800

    move render to main module, adjust camera

--- modules/scene.js
+++ modules/scene.js
@@ -1,5 +1,5 @@
 
-export var canvas = d3.select("body").append("canvas")
+var canvas = d3.select("body").append("canvas")
   .attr("width", window.innerWidth)
   .attr("height", window.innerHeight)
   .style("position", "absolute")
@@ -8,38 +8,16 @@ export var canvas = d3.select("body").append("canvas")
 
 canvas.node().getContext("webgl");
 
-var renderer = new THREE.WebGLRenderer({canvas: canvas.node(), antialias: true});
+export var renderer = new THREE.WebGLRenderer({canvas: canvas.node(), antialias: true});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 5000);
-camera.position.z = 1000;
+export var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 2000);
+camera.position.z = 2000;
 
 export var scene = new THREE.Scene();
 
 var light = new THREE.HemisphereLight('#ffffff', '#666666', 1.5);
 light.position.set(0, 3000, 0);
 scene.add(light);
-
-// var stats = new Stats();
-// stats.domElement.style.position = 'absolute';
-// stats.domElement.style.bottom = '45px';
-// document.body.appendChild(stats.domElement);
-
-window.addEventListener('resize', onWindowResize, false);
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function animate() {
-  // stats.update();
-
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-
-animate();
